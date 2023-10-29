017e71331fbbb0bd3c5c6c8139ddc7b77a55a338
Zumrud03 <nusretovazumrud@gmail.com>
2023-10-30 02:53:17 +0400

    cb function

---
+++ assets/script.js
@@ -0,0 +1,69 @@
+// ? 1.Istenilen sayda  gelen datalarin Ortalamasini veren Js function yazin.Call back istifade edeceksiz.
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const ededi_orta = () => {
+//   let sum = 0;
+//   let ededi_orta = 0;
+//   for (let i = 0; i <= arr.length; i++) {
+//     sum = sum + i;
+//   }
+//   ededi_orta = sum / arr.length;
+//   return ededi_orta;
+// };
+
+// console.log(ededi_orta(arr));
+
+// ? 2.Istenilen sayda gelen datalarin cut olanlarinin toplamini tapan Js function yazin.Call back istifade edeceksiz.
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const sum = () => {
+//   let sum = 0;
+//   for (let i = 0; i <= arr.length; i++) {
+//     if (i % 2 === 0) {
+//       sum = sum + i;
+//     }
+//   }
+//   return sum;
+// };
+
+// console.log(sum(arr));
+
+// ? 3.Istenilen sayda gelen datalarin tek olanlarinin toplamini tapan Js function yazin.Call back istifade edeceksiz.
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const sum = () => {
+//   let sum = 0;
+//   for (let i = 0; i <= arr.length; i++) {
+//     if (i % 2 === 1) {
+//       sum = sum + i;
+//     }
+//   }
+//   return sum;
+// };
+
+// console.log(sum(arr));
+
+// ? 4.Istenilen sayda gelen datalarin cut olanlarin toplamini tek olanlarinin toplamina olan hasilini tapan Js function yazin.Call back istifade edeceksiz.
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const sum = () => {
+//   let sum_odd = 0;
+//   let sum_even = 0;
+//   let hasil = 0;
+//   for (let i = 0; i <= arr.length; i++) {
+//     if (i % 2 === 0) {
+//       sum_odd = sum_odd + i;
+//     } else {
+//       sum_even = sum_even + i;
+//     }
+//   }
+//   hasil = sum_odd * sum_even;
+
+//   return hasil;
+// };
+
+// console.log(sum(arr));
